33fb80cddd59ad070765f0b1e1e6e848218daac8
exceldeo <excel.deo99@gmail.com>
2024-06-26 22:03:53 +0700

    init project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <div className="text-center">
+                <h1 className="text-5xl font-bold">Hello, Laravel!</h1>
+                <p className="text-lg text-gray-600">This is a React component rendered by Vite.</p>
+            </div>
+        </div>
+    );
+}
